96a4ae2ad0eb9770e9d71ae29d34d5b871719a96
Doug Roeper <Dig-Doug@users.noreply.github.com>
2021-02-18 06:09:01 -0800

    Fixes a bug when handling proto files inside directories without BUILD files. (#143)

--- test/test_bundling.ts
+++ test/test_bundling.ts
@@ -1,4 +1,5 @@
+export {DeliveryPerson} from 'rules_typescript_proto/test/proto/common/delivery_person_pb';
+export {Pizza, PizzaSize} from 'rules_typescript_proto/test/proto/common/pizza_pb';
+export {InsideDir} from 'rules_typescript_proto/test/proto/dir/inside_pb';
 export {alllowercase, ALLUPPERCASE, lowerCamelCase, m24M_, M2_M, M2M, M2M_, m42_M, M9, m_22M, M_2M, snake_case_snake_case, Upper_snake_Case, UpperCamelCase,} from 'rules_typescript_proto/test/proto/naming_styles_pb';
-export { DeliveryPerson } from 'rules_typescript_proto/test/proto/common/delivery_person_pb';
-export { Pizza, PizzaSize } from 'rules_typescript_proto/test/proto/common/pizza_pb';
-export { PizzaService, PizzaServiceClient } from 'rules_typescript_proto/test/proto/pizza_service_pb_service';
+export {PizzaService, PizzaServiceClient} from 'rules_typescript_proto/test/proto/pizza_service_pb_service';
